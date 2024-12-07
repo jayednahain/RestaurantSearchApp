cb4923603df485d07264a611986800d5425f328d
jayednahian <jayednahian@yahoo.com>
2024-12-07 23:31:23 +0600

    9-100: completed - api request sended

--- App/Views/SearchView.js
+++ App/Views/SearchView.js
@@ -1,19 +1,32 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import SearchBar from '../Components/SearchBar'
+import DummyJsonService from '../Service/DummyJsonService';
 
 export default function SearchView() {
-    const [searchKeyWord, setSearchKeyWord] = useState("")
+    const [searchKeyWord, setSearchKeyWord] = useState("");
+    const [responseList, setResponseList] = useState("");
+    //'https://dummyjson.com/products/search?q=phone'
+    
+    const searchApi = async () => {
+        const response = await DummyJsonService.get('/search',{
+            q : searchKeyWord
+        })
+        console.log("response : ", JSON.stringify(response.data.products.length))
+        setResponseList(response.data.products)
+    }
+    
 
     return (
-        <View>
+        <View style ={{flex:1}}>
             <SearchBar
                 searchKeyWord={searchKeyWord}
                 onTextChange={(text) => setSearchKeyWord(text)}
-                onTextSubmitted={()=>{
-                    console.warn("SUBMITTED")
+                onTextSubmitted={() => {
+                    searchApi()
                 }}
             />
+           <Text> `total {responseList.length}`</Text>
         </View>
     )
 }
