890c32a9d9ed5c8027f2ff982927a6df883aa767
jayednahian <jayednahian@yahoo.com>
2025-03-23 09:40:05 +0600

    login page working

--- App/Views/Authentication/Login.js
+++ App/Views/Authentication/Login.js
@@ -5,30 +5,28 @@ import { ContextProviderUser, UserContext } from '../../CustomContext';
 
 export default function Login() {
 
-  const [userName, setUserName] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [ userName , setUserName ] = useState(null);
+  const [ password , setPassword ] = useState(null);
   const { setUser } = useContext(UserContext);
 
-  const handleOnPressSubmit = ()=>{
-    console.log("");
-    setUser({userName,password});
+  const handleOnPressSubmit = () => {
+    setUser({ userName, password });
   }
 
   return (
     <View>
       <TextInput 
-        style={{ borderWidth: 0.5, borderColor: 'black', padding: 5, margin: 5, color: "black" }} 
-        placeholder='user name' 
+        style={{ borderWidth: 0.5, borderColor: 'black', padding: 5, margin: 5, color: "black" }}
+        placeholder='user name'
         placeholderTextColor={"black"}
-        onChangeText={value=>setUserName(value)}
-
-        />
-      <TextInput 
-        style={{ borderWidth: 0.5, borderColor: 'black', padding: 5, margin: 5, color: "black" }} 
-        placeholder='password' 
-        placeholderTextColor={"black"} 
-        onChangeText={value=>setPassword(value)}
-        />
+        onChangeText={value => setUserName(value)}
+      />
+      <TextInput
+        style={{ borderWidth: 0.5, borderColor: 'black', padding: 5, margin: 5, color: "black" }}
+        placeholder='password'
+        placeholderTextColor={"black"}
+        onChangeText={value => setPassword(value)}
+      />
       <Button title='submit' onPress={handleOnPressSubmit} />
     </View>
   )
